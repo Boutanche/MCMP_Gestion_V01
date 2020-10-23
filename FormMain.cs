c11caf6b593cb011414cfc2af8a69c2aa95f732c
Benoit BERTRAND <benoit.bertrand@live.fr>
2020-10-23 20:48:16 +0200

    Ajoutez des fichiers projet.

---
+++ FormMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCMP_Gestion_V01
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
